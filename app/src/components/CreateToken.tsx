bb4c94b9e728bba7c6753f154ffe6539ed05972d
kiet.huynh <kiet.huynh@s3corp.com.vn>
2023-10-20 17:10:56 +0700

    Update mint function

--- app/src/components/CreateToken.tsx
+++ app/src/components/CreateToken.tsx
@@ -1,10 +1,19 @@
 import { FC, useCallback, useState } from "react";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { Transaction, PublicKey } from "@solana/web3.js";
-import { createFungible } from "@metaplex-foundation/mpl-token-metadata";
+import {
+  TokenStandard,
+  createFungible,
+  createV1,
+  mintV1,
+} from "@metaplex-foundation/mpl-token-metadata";
 
 import { useUmi } from "pages/useUmi";
-import { generateSigner, percentAmount } from "@metaplex-foundation/umi";
+import {
+  generateSigner,
+  percentAmount,
+  transactionBuilder,
+} from "@metaplex-foundation/umi";
 import { notify } from "utils/notifications";
 
 export const CreateToken: FC = () => {
@@ -12,22 +21,35 @@ export const CreateToken: FC = () => {
   const [tokenName, setTokenName] = useState("");
   const [symbol, setSymbol] = useState("");
   const [metadata, setMetadata] = useState("");
+  const [amount, setAmount] = useState(0);
   const umi = useUmi();
 
   const onClick = useCallback(
     async (form) => {
       const mint = generateSigner(umi);
-      await createFungible(umi, {
-        mint,
-        name: form.tokenName,
-        uri: form.metadata,
-        symbol: form.symbol,
-        sellerFeeBasisPoints: percentAmount(5.5),
-      }).sendAndConfirm(umi);
+      let builder = transactionBuilder()
+        .add(
+          createFungible(umi, {
+            mint,
+            name: form.tokenName,
+            uri: form.metadata,
+            symbol: form.symbol,
+            sellerFeeBasisPoints: percentAmount(5.5),
+          })
+        )
+        .add(
+          mintV1(umi, {
+            mint: mint.publicKey,
+            amount: form.amount,
+            tokenStandard: TokenStandard.Fungible,
+          })
+        );
+      const confirmResult = await builder.sendAndConfirm(umi);
+      console.log("mint", mint.publicKey);
       notify({
         type: "success",
         message: "Successfully",
-        txid: mint.publicKey,
+        txid: confirmResult.signature.toString(),
       });
     },
     [umi]
@@ -52,6 +74,12 @@ export const CreateToken: FC = () => {
         onChange={(e) => setSymbol(e.target.value)}
       />
       <input
+        type="number"
+        className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        placeholder="Amount"
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <input
         type="text"
         className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
         placeholder="Metadata Url"
@@ -64,6 +92,7 @@ export const CreateToken: FC = () => {
             metadata: metadata,
             symbol: symbol,
             tokenName: tokenName,
+            amount: amount,
           })
         }
       >
